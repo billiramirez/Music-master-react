7fcc5b753adfad02f81d227c9b834930a9c8feb2
Billi Ramirez <ramirezbilly42@outlook.com>
2017-09-08 12:24:58 -0600

    adding the App component

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+
+    render() {
+        return <div>
+            <div className="App-title">Music Master from App</div>
+            <div>
+                <input placeholder="Search an artist... " />
+                <button>button</button>
+            </div>
+
+            <div className="Profile">
+                <div>Artist Picture</div>
+                <div>Artist Name</div>
+            </div>
+
+            <div className="Gallery">
+                Gallery
+            </div>
+          </div>;
+    }
+}
+
+export default App;
