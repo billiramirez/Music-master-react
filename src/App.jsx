b99c639ba56427857afbc11abf2faa9b5c6512d2
Billi Ramirez <ramirezbilly42@outlook.com>
2017-09-08 14:58:21 -0600

    searching for artist logic and function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
 import React, {Component} from 'react';
-
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 
 class App extends Component {
 
+    constructor(props){
+        super(props);
+
+
+    this.state = { 
+        query: "" 
+    };
+
+}
+
+search(){
+    //console.log(`This state: ${this.state.query}`);
+    console.log('this.state ', this.state);
+}
+
+
     render() {
-        return <div>
+        return <div className="App">
             <div className="App-title">Music Master from App</div>
-            <div>
-                <input placeholder="Search an artist... " />
-                <button>button</button>
-            </div>
+            <FormGroup>
+              <InputGroup>
+                <FormControl type="text" 
+                placeholder="Search for an Artist" 
+                value={this.state.query}
+                onChange={event => {this.setState({query: event.target.value})}}
+                onKeyPress={event => {
+                    if(event.key === 'Enter'){
+                        this.search()
+                    }
+                }}
+                />
+                <InputGroup.Addon onClick={()=> this.search()}>
+                    <Glyphicon glyph="search"></Glyphicon>
+                </InputGroup.Addon>
+              </InputGroup>
+            </FormGroup>
 
             <div className="Profile">
-                <div>Artist Picture</div>
-                <div>Artist Name</div>
+              <div>Artist Picture</div>
+              <div>Artist Name</div>
             </div>
 
-            <div className="Gallery">
-                Gallery
-            </div>
+            <div className="Gallery">Gallery</div>
           </div>;
     }
 }
